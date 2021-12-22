5fd082750d3d3e0ab21048f0b3e721744fc34910
alkatel3 <74428339+alkatel3@users.noreply.github.com>
2021-12-22 17:16:06 +0200

    Add routes to app

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,9 @@
 const app = require('./server');
 const sequelize = require('./db');
 
+const router = require('./routes');
+router(app);
+
 (async () => {
   await sequelize.sync({ force: false });
 
